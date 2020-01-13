ed11c319f72bf89ca2f5170832e078c9a6eee160
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-12 16:08:45 -0800

    Add github api request and file link to scripts in HTML

---
+++ app.js
@@ -0,0 +1,43 @@
+//===================================
+//AJAX REQUEST METHOD
+
+// const Http = new XMLHttpRequest();
+// const url = "https://api.github.com/users/jamisoncozart/repos";
+// Http.open("GET", url);
+// Http.send();
+
+// Http.onreadystatechange = (err) => {
+//   if(this.readyState==4 && this.status==200) {
+//     console.log(Http.responseText);
+//   } else {
+//     console.log(err);
+//   }
+// }
+
+//==================================
+//AJAX JQUERY REQUEST METHOD
+
+// $(document).ready(function() {
+//   const Url = "https://api.github.com/users/jamisoncozart/repos";
+//   $.ajax({
+//     url: Url,
+//     type: "GET",
+//     success: function(result) {
+//       console.log(result)
+//     },
+//     error: function(error) {
+//       console.log(`Error: ${error}`);
+//     }
+//   })
+// })
+
+//=================================
+//JQUERY REQUEST METHOD
+
+$(document).ready(function() {
+  const Url = "https://api.github.com/users/jamisoncozart/repos";
+  $.get(Url, function(data, status) {
+    console.log(data);
+  })
+})
+
